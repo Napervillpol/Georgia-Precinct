dc6cc634a831d48ece7fdc7e4b510314f4149bb2
Naperville Nate <napervillepolitics@gmail.com>
2022-11-21 00:20:32 -0600

    added stats

--- Georgia_Precinct.py
+++ Georgia_Precinct.py
@@ -64,6 +64,157 @@ def get_candidate(xpath,name):
     df.insert(0, "ID", name +" " +df['Counties'])
 
     return df
+
+def safediv(x,y):
+    try:
+        return x/y
+    except ZeroDivisionError:
+        return 0
+
+def write_to_excel(race,race_name):
+    writer = pd.ExcelWriter('GA_'+race_name+ '.xlsx', engine='xlsxwriter')
+
+    race.mail.to_excel(writer,sheet_name="Mail",index=False)
+    race.eday.to_excel(writer,sheet_name="Election Day",index=False)
+    race.advance.to_excel(writer,sheet_name="Advance",index=False)
+    race.prov.to_excel(writer,sheet_name="Provisonal",index=False)
+    race.total.to_excel(writer,sheet_name="Total",index=False)
+
+    writer.save()
+class race:
+    mail=[]
+    eday=[]
+    advance=[];
+    prov=[]
+    total=[]
+    def __init__(self, mail,eday,advance,prov,total):
+        self.mail=mail
+        self.eday=eday
+        self.advance=advance
+        self.prov=prov
+        self.total=total
+
+def assign_race(Dem,Rep,Dem_name,Rep_name):
+       
+    #Mail 
+    Dem_mail = Dem[['ID','Absentee by Mail Votes']]
+    Dem_mail.columns=['County',Dem_name]
+
+    Rep_mail = Rep[['ID','Absentee by Mail Votes']]
+    Rep_mail.columns=['County',Rep_name]
+    mail = Dem_mail.merge(Rep_mail, on='County')
+    calculations(mail,Dem_name,Rep_name)
+   
+    #Election day
+    Dem_eday = Dem[['ID','Election Day Votes']]
+    Dem_eday.columns=['County',Dem_name]
+
+    Rep_eday = Rep[['ID','Election Day Votes']]
+    Rep_eday.columns=['County',Rep_name]
+    eday = Dem_eday.merge(Rep_eday, on='County')
+    calculations(eday,Dem_name,Rep_name)
+
+    #Advance
+    Dem_advance = Dem[['ID','Advance Voting Votes']]
+    Dem_advance.columns=['County',Dem_name]
+
+    Rep_advance  = Rep[['ID','Advance Voting Votes']]
+    Rep_advance.columns=['County',Rep_name]
+    advance = Dem_advance.merge(Rep_advance, on='County')
+    calculations(advance,Dem_name,Rep_name)
+
+    #Provisonal
+    Dem_prov= Dem[['ID','Provisional Votes']]
+    Dem_prov.columns=['County',Dem_name]
+
+    Rep_prov = Rep[['ID','Provisional Votes']]
+    Rep_prov.columns=['County',Rep_name]
+    prov = Dem_prov.merge(Rep_prov, on='County')
+    calculations(prov,Dem_name,Rep_name)
+
+     #Total
+    Dem_total= Dem[['ID','Total']]
+    Dem_total.columns=['County',Dem_name]
+
+    Rep_total = Rep[['ID','Total']]
+    Rep_total.columns=['County',Rep_name]
+    total = Dem_total.merge(Rep_total, on='County')
+    calculations(total,Dem_name,Rep_name)
+
+    Race = race(mail,eday,advance,prov,total)
+    return Race;
+
+def calculations(df,Dem_name,Rep_name):
+   
+    df[Dem_name]=df[Dem_name].astype(str)
+    df[Rep_name]=df[Rep_name].astype(str)
+    
+    df[Dem_name]=df[Dem_name].str.replace(',','')
+    df[Rep_name]=df[Rep_name].str.replace(',','')
+
+    df[Dem_name]=df[Dem_name].astype(int)
+    df[Rep_name]=df[Rep_name].astype(int)
+    
+    df.insert(3, "Total", df[Dem_name]+df[Rep_name])
+    df.insert(4, "Net Votes", df[Dem_name]-df[Rep_name])
+    df.insert(5, Dem_name+" Pct", df[Dem_name]/(df[Dem_name]+df[Rep_name]))
+    df.insert(6, Rep_name+" Pct", df[Rep_name]/(df[Dem_name]+df[Rep_name]))
+    df.insert(7, "Margin",(df[Dem_name]/(df[Dem_name]+df[Rep_name])) -(df[Rep_name]/(df[Dem_name]+df[Rep_name])))
+
+def calculate_shift(df_2022,df_2020):
+     
+     df_2022.mail.insert(8, "Pct Shift",df_2022.mail["Margin"]-df_2020.mail["Margin"])
+     df_2022.mail.insert(9, "Turnout",df_2022.mail["Total"]/df_2020.mail["Total"])
+
+     df_2022.eday.insert(8, "Pct Shift",df_2022.eday["Margin"]-df_2020.eday["Margin"])
+     df_2022.eday.insert(9, "Turnout",df_2022.eday["Total"]/df_2020.eday["Total"])
+
+     df_2022.advance.insert(8, "Pct Shift",df_2022.advance["Margin"]-df_2020.advance["Margin"])
+     df_2022.advance.insert(9, "Turnout",df_2022.advance["Total"]/df_2020.advance["Total"])
+
+     df_2022.prov.insert(8, "Pct Shift",df_2022.prov["Margin"]-df_2020.prov["Margin"])
+     df_2022.prov.insert(9, "Turnout",df_2022.prov["Total"]/df_2020.prov["Total"])
+
+     df_2022.total.insert(8, "Pct Shift",df_2022.total["Margin"]-df_2020.total["Margin"])
+     df_2022.total.insert(9, "Turnout",df_2022.total["Total"]/df_2020.total["Total"])
+
+def Statmodels(Previous_race,Current_race,Current_name,Title,w):
+    
+  
+    
+    plt.title(Title)
+    plt.xlabel("Warnock General Pct")
+    plt.ylabel("Warnock Runoff Pct")
+    print(Previous_race)
+    plt.scatter(Previous_race['Warnock Pct'],Current_race[Current_name],w)
+   
+  
+
+    x = Previous_race['Warnock Pct']
+    y = Current_race['Warnock Pct']
+    z = w
+    
+ 
+    wls_model = sm.WLS(y,x,z)
+    results = wls_model.fit()
+    
+    
+    plt.plot(x,results.fittedvalues)
+    
+    xpoint = pd.DataFrame(x, columns=['Warnock Pct'])
+    ypoint = pd.DataFrame(results.fittedvalues, columns=['expected'])
+    newline = pd.merge(xpoint, ypoint, left_index=True, right_index=True)
+    newline =newline.sort_values(by=['expected']).reset_index(drop=True)
+    
+    swing = (newline.iloc[0][1] - newline.iloc[0][0] + newline.iloc[-1][1] - newline.iloc[-1][0])
+    print("{} swing: {:.1%}".format(Title,swing))
+    x = np.linspace(0,1,5)
+    y = x
+   
+    plt.grid()
+    plt.plot(x, y, '-r', label='y=x+1')
+
+    plt.show()
 def get_candidate_precinct(candidate,xpath):
    
     candidate.append(get_data( "https://results.enr.clarityelections.com//GA/Appling/115467/313148/reports/detailxml.zip",xpath,'Appling')) 
@@ -228,11 +379,15 @@ def get_candidate_precinct(candidate,xpath):
     candidate=pd.concat(candidate)
     return candidate
 
-Warnock=[]
+Warnock = pd.read_csv("Data/Warnock.csv")
+Walker = pd.read_csv("Data/Walker.csv")
+Abrams = pd.read_csv("Data/Abrams.csv")
+Kemp = pd.read_csv("Data/Kemp.csv")
 
-Warnock = get_candidate_precinct(Warnock,".//Choice[@text='Raphael Warnock (I) (Dem)']")
+Senate =assign_race(Warnock,Walker,"Warnock","Walker")
+Governor =assign_race(Abrams,Kemp,"Abrams","Kemp")
+calculate_shift(Governor,Senate)
 
+write_to_excel(Governor,"Governor")
 
-Warnock.to_csv("Warnock.csv",index=False)
-print(Warnock)
-
+Statmodels(Senate.total,Governor.total,"Warnock Pct","GA",1)
